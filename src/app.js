a539c9bcee1a64076770747976cd33f55e87a9af
Fernando Sartorius <92523759+Sartorius11@users.noreply.github.com>
2024-11-06 10:28:36 +0000

    1Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//funcion para generar la frase random
+function fraseRandom() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  //0           1            2             3
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomFrase = `${who[Math.floor(Math.random() * who.length)]} ` +
+    `${action[Math.floor(Math.random() * action.length)]} ` +
+    `${what[Math.floor(Math.random() * what.length)]} ` +
+    `${when[Math.floor(Math.random() * when.length)]};`;
+
+
+  //hace que donde esta el id en el html aparezca la frase random
+  document.getElementById("excuse").innerHTML = randomFrase;
+
+}
+//cuando los elementos de la pagina esten cargados
+//ejecuta la funcion fraseRandom
+window.onload = function () {
+  fraseRandom();
+
+
 };
